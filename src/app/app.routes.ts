11dcf8f820798433de53d936956ad9a0b418e14f
bwright-design <35242420+bwright-design@users.noreply.github.com>
2025-02-26 20:29:53 -0600

    new stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {DarkNetComponent} from "./pages/dark-net/dark-net.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',   redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'data', component: DarkNetComponent},
+];

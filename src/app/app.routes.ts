b83fbe70587d4ac500d3e14729422473f514e090
bwright-design <35242420+bwright-design@users.noreply.github.com>
2025-02-28 16:36:43 -0600

    new stuff 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {DarkNetComponent} from "./pages/dark-net/dark-net.component";
+import {MapComponent} from "./pages/map/map.component";
+import {ShipComponent} from "./pages/ship/ship.component";
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'data', component: DarkNetComponent},
+    { path: 'net', component: DarkNetComponent},
+    { path: 'map', component: MapComponent},
+    { path: 'ship', component: ShipComponent},
 ];
--- src/app/pages/ship/ship.component.ts
+++ src/app/pages/ship/ship.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ShipmodelComponent} from "./shipmodel/shipmodel.component";
+import {CardComponent} from "../../components/card/card.component";
 
 @Component({
   selector: 'app-ship',
-  imports: [],
+  imports: [ShipmodelComponent, CardComponent],
   templateUrl: './ship.component.html',
+  standalone: true,
   styleUrl: './ship.component.scss'
 })
 export class ShipComponent {
